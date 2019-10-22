558398a73152e508d60221176987a088078e3ba2
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-10-22 12:31:59 -0400

    Adds e2e test for WelcomeScreen, adds onClick props, moves components to separate folders

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import WelcomeScreen from './welcome-screen.jsx';
+import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+
+const onStartGameClick = () => {
+  // TODO: implement
+};
 
 const App = (props) => {
   const {time, numErrors} = props;
 
-  return <WelcomeScreen time={time} numErrors={numErrors}
+  return <WelcomeScreen time={time} numErrors={numErrors} onClick={onStartGameClick}
   />;
 };
 
